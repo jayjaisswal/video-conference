f49a48ada30f9fd1e9578523ab702b5f5ce5a3b7
jayjaisswal <kumarprince13833@gmail.com>
2025-07-11 23:32:23 +0530

    authcontext fixing problem

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import  {BrowserRouter} from 'react-router-dom'
 import toast, { Toaster } from 'react-hot-toast'
-import {AuthProvider} from '../src/contexts/authContext.jsx'
+import {AuthProvider} from '../src/contexts/AuthContext.jsx'
 createRoot(document.getElementById('root')).render(
   
   <StrictMode>
